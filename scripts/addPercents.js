df89d1eeb76d5fa3fb024d13bf3da0597f2a38ea
tymofii-s <t967887744@gmail.com>
2025-03-14 19:44:24 +0200

    add percents list

--- scripts/addPercents.js
+++ scripts/addPercents.js
@@ -1,9 +1,16 @@
+let globalData;
 function addPercentsP(data) {
-    alert(data)
+    globalData = data
     document.getElementById("percents").innerHTML = `
-        100% прав. відп.: ${data[0] *100} <br>
-        75% прав. відп.: ${data[1] *100} <br>
-        50% прав. відп.: ${data[2] *100} <br>
-        25% прав. відп.: ${data[3] *100} <br>
+        100% прав. відп.: ${getNum(0)} <br>
+        75% прав. відп.: ${getNum(1)} <br>
+        50% прав. відп.: ${getNum(2)} <br>
+        25% прав. відп.: ${getNum(3)} <br>
     `
+}
+
+function getNum(index) {
+    let data = globalData
+
+    return (data[index] * 100).toFixed(2)
 }
